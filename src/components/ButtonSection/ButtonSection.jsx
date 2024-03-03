8b2c8a98ffe50f205bbeaa1993990e083ac87fa4
ArtPar <am.parunakyan@gmail.com>
2024-03-03 22:57:37 +0300

    Сформировал структуру будущего сайта

---
+++ src/components/ButtonSection/ButtonSection.jsx
@@ -0,0 +1,10 @@
+import Button from '../Button/Button'
+
+export default function ButtonSection() {
+    return (
+        <section>
+            <Button />
+            <Button />
+        </section>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+
+export default function Header () {
+    const [time, setTime] = useState(new Date())
+
+    setInterval(() => setTime(new Date()), 1000)
+
+    return (
+        <header>
+            <h3>Buy&Sell</h3>
+
+            <span>{time.toLocaleTimeString()}</span>
+        </header>
+    )
+}
